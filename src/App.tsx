c2139bd89ab152741af90a16eeda0a8d455fc63d
Emmett Childs <emmettchilds@thegobospot.com>
2022-03-01 13:54:41 +0800

    deciding what to build this into lmao

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+const name: string = "Joe";
+const amount: number = 69;
+const status: boolean = false;
+
+function App() {
+    return (
+        <div className="App">
+            <h1>Hello</h1>
+        </div>
+    );
+}
+
+export default App;
